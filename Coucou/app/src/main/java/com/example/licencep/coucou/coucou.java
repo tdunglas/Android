35e0467b1f2e4f6d2982a540c829b58c7cdd7f94
licencep <licencep@samar08.cnam.fr>
2018-03-30 16:35:57 +0200

    tp android master sems

---
+++ Coucou/app/src/main/java/com/example/licencep/coucou/coucou.java
@@ -0,0 +1,13 @@
+package com.example.licencep.coucou;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class coucou extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coucou);
+    }
+}
